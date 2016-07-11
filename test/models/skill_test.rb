9dc8ccf0123a0a0a0fe2dad8e05d9076de508442
Lockeriano <patryk.skrzyniarz19@gmail.com>
2016-07-11 21:24:51 +0200

    Add skill to user

--- test/models/skill_test.rb
+++ test/models/skill_test.rb
@@ -1,8 +1,4 @@
 class SkillTest < ActiveSupport::TestCase
-     @skill.level = 6
-     assert_not @skill.valid?
-  end
-
 	test 'should be unique for given fighter' do
 		puts @fighter.skills.create(name: "Smite", level: 2)
 		assert_not @skill.valid?
